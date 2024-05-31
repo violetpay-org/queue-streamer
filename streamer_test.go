fa4a1c48a1d1dfe81d4c82547c54b02c8289421a
Jaewook Lee <wookboy00@naver.com>
2024-05-31 19:20:47 +0900

    Test: TopicStreamer test uses PassThroughSerializer

--- streamer_test.go
+++ streamer_test.go
@@ -1,27 +1,19 @@
 package qstreamer_test
 
 import (
-	qstreamer "github.com/violetpay-org/queue-streamer"
+	"github.com/violetpay-org/queue-streamer"
+	"sync"
 	"testing"
 	"time"
 )
 
-type TestSerializer struct {
-}
-
-func (ts *TestSerializer) MessageToProduceMessage(value string) string {
-	return value
-}
-
-func NewTestSerializer() *TestSerializer {
-	return &TestSerializer{}
-}
+var wg sync.WaitGroup = sync.WaitGroup{}
 
 func TestTopicStreamer(t *testing.T) {
 	brokers := []string{"b-3.vpkafkacluster2.zy10lp.c3.kafka.ap-northeast-2.amazonaws.com:9092", "b-2.vpkafkacluster2.zy10lp.c3.kafka.ap-northeast-2.amazonaws.com:9092", "b-1.vpkafkacluster2.zy10lp.c3.kafka.ap-northeast-2.amazonaws.com:9092"}
 	endTopic := qstreamer.NewTopic("te", 3)
 	endTopic2 := qstreamer.NewTopic("te2", 5)
-	serializer := NewTestSerializer()
+	serializer := qstreamer.NewPassThroughSerializer()
 	spec := qstreamer.NewStreamConfig(serializer, endTopic)
 	spcc2 := qstreamer.NewStreamConfig(serializer, endTopic2)
 
@@ -34,7 +26,7 @@ func TestTopicStreamer(t *testing.T) {
 	topicStreamer.AddConfig(spcc2)
 
 	topicStreamer.Run()
-	defer topicStreamer.StopAll()
-	time.Sleep(1000 * time.Second)
-	topicStreamer.StopAll()
+	defer topicStreamer.Stop()
+	time.Sleep(100 * time.Second)
+	topicStreamer.Stop()
 }
